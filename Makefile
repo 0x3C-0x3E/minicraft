4b54e75136c03a62a19d11077f37ff7afd5b5560
0x3C-0x3E <rbng0094@gmail.com>
2025-05-29 18:30:03 +0200

    working things

---
+++ Makefile
@@ -0,0 +1,39 @@
+# Compiler and flags
+CC := gcc
+CFLAGS := -Wall -std=c11 `sdl2-config --cflags`
+LDFLAGS := `sdl2-config --libs` -lSDL2_image
+
+# Directories
+SRC_DIR := src
+BUILD_DIR := build
+BIN := $(BUILD_DIR)/game
+
+# Find all .c source files in SRC_DIR recursively
+SRC_FILES := $(shell find $(SRC_DIR) -name '*.c')
+
+# Replace SRC_DIR with BUILD_DIR and .c with .o for object files
+OBJ_FILES := $(patsubst $(SRC_DIR)/%.c, $(BUILD_DIR)/%.o, $(SRC_FILES))
+
+# Default target
+all: $(BIN)
+
+# Link all object files into the binary
+$(BIN): $(OBJ_FILES)
+	@mkdir -p $(dir $@)
+	$(CC) $^ -o $@ $(LDFLAGS)
+
+# Compile .c files into .o files
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(dir $@)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+# Clean compiled files
+clean:
+	rm -rf $(BUILD_DIR)
+
+# Convenience alias
+run: all
+	./$(BIN)
+
+.PHONY: all clean run
+
--- src/main.c
+++ src/main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
-int main()
-{
-    printf("Hello, World!\n");
+int main(int argc, char* argv[]) {
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Could not init SDL\n");
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL Test Window",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          800, 600,
+                                          SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 135, 206, 250, 255); // Sky blue
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000); // Show window for 3 seconds
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
+
 }
